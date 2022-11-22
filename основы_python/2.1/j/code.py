53481cfb526e0c30ce08a351fe000dd137f5d220
0xMihalich <bayan-mobile@list.ru>
2022-11-22 11:02:39 +1000

    Create code.py

---
+++ основы_python/2.1/j/code.py
@@ -0,0 +1,9 @@
+babyName = input()
+selfNumber = input()
+groupNum = selfNumber[0]
+bedNum = selfNumber[1]
+babyNum = selfNumber[2]
+print(f'''Группа №{groupNum}.
+{babyNum}. {babyName}.
+Шкафчик: {selfNumber}.
+Кроватка: {bedNum}.''')
