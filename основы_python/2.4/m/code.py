1b8275a5bea1dae27fb322bef706aac96602fd54
0xMihalich <bayan-mobile@list.ru>
2022-11-22 11:45:45 +1000

    Create code.py

---
+++ основы_python/2.4/m/code.py
@@ -0,0 +1,6 @@
+weight = int(input())
+height = int(input())
+square = [str(j) for j in range(1, weight * height + 1)]
+lens = len(square[-1])
+for i in range(weight):
+    print(' '.join(' ' * (lens - len(j)) + j for j in square[i::weight]))
