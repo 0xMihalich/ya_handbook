dee8a5e9ae767081e9e6e35dd50cc8094512ac38
0xMihalich <bayan-mobile@list.ru>
2022-11-22 12:52:42 +1000

    Update code.py

--- основы_python/3.1/s/code.py
+++ основы_python/3.1/s/code.py
@@ -1,12 +1,4 @@
 polskiu = []
- 
- 
-def fac(n):
-    if n == 0:
-        return 1
-    return fac(n - 1) * n
- 
- 
 s = input().split()
 for x in s:
     if x == '+':
@@ -21,25 +13,6 @@ for x in s:
         g = polskiu.pop()
         z = polskiu.pop()
         polskiu.append(g * z)
-    elif x == '#':
-        polskiu.append(polskiu[-1])
-    elif x == '@':
-        polskiu.append(polskiu[-2])
-        polskiu.append(polskiu[-2])
-        polskiu.append(polskiu[-5])
-        del polskiu[-4]
-        del polskiu[-4]
-        del polskiu[-4]
-    elif x == '/':
-        g = polskiu.pop()
-        z = polskiu.pop()
-        polskiu.append(z // g)
-    elif x == '~':
-        g = polskiu.pop()
-        polskiu.append(-g)
-    elif x == '!':
-        g = polskiu.pop()
-        polskiu.append(fac(g))
     else:
         polskiu.append(int(x))
 print(polskiu[0])
