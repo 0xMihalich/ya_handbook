89ddbb781ecd8b7cc09a6ddf31c9fd4fcb489f95
0xMihalich <bayan-mobile@list.ru>
2022-11-23 21:06:33 +1000

    Update code.py

--- основы_python/3.4/i/code.py
+++ основы_python/3.4/i/code.py
@@ -1,3 +1,4 @@
+# к сожалению, не смог придумать, как решить эту задачу при помощи itertools.product, поэтому решил через list comprehension
 col = int(input())
 for row in (list(str(j) for j in range(1 * i, (col + 1) * i, i)) for i in range(1, col + 1)):
     print(' '.join(row))
