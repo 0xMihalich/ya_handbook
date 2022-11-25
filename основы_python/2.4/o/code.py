087c602f432ded6dfbb03b4046b73e0bb70bdc5a
0xMihalich <bayan-mobile@list.ru>
2022-11-26 00:05:09 +1000

    Update code.py

--- основы_python/2.4/o/code.py
+++ основы_python/2.4/o/code.py
@@ -1,1 +1,12 @@
-# пока не решено
+cols = int(input())
+rows = int(input())
+result = [[] for i in range(cols)]
+size = len(str(cols * rows))
+nums = [f'{j:>{size}}' for j in range(1, rows * cols + 1)]
+reverse = 1
+for col in [nums[i:i + cols] for i in range(0, len(nums), cols)]:
+    for n, row in enumerate(col[::reverse]):
+        result[n].append(row)
+    reverse = -reverse
+for row in result:
+    print(' '.join(row))
