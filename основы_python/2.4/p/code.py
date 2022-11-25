8f761887f852eded1fb4deb0f95949aa43515fbe
0xMihalich <bayan-mobile@list.ru>
2022-11-26 02:15:24 +1000

    Update code.py

--- основы_python/2.4/p/code.py
+++ основы_python/2.4/p/code.py
@@ -1,1 +1,12 @@
-# пока не решено
+# пробовал разные варианты, в итоге пришел к этому коду, тем не менее тесты все-равно не проходит, исключать, что в тестах нет ошибки нельзя,
+# поскольку ранее уже было замечено и исправлено через обращение в ТП две ошибки в тестах.
+
+cols = int(input())
+rows = int(input())
+size = len(str(cols * rows))
+for i in range(1, cols + 1):
+    nums = (f'{j: >{size}}' for j in range(i, i * rows + 1, i))
+    result = '|'.join(f'{num: ^{rows}}' for num in nums)
+    print(result)
+    if i < cols:
+        print('-' * len(result))
