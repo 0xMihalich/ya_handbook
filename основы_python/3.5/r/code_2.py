ef0408d92f528a7d5fb3d8ce1d86fbf63309651e
0xMihalich <bayan-mobile@list.ru>
2022-11-26 17:37:58 +1000

    Create code_2.py

---
+++ основы_python/3.5/r/code_2.py
@@ -0,0 +1,14 @@
+from math import ceil
+
+
+def filesize(size, value=0):
+    sizes = {0: 'Б', 1: 'КБ', 2: 'МБ', 3: 'ГБ', 4: 'ТБ'}
+    if size > 1024:
+        value += 1
+        return filesize(size / 1024, value)
+    return f'{ceil(size)}{sizes[value]}'
+
+
+with open(input(), "rb") as file:
+    file.seek(0, 2)
+    print(filesize(file.tell()))
