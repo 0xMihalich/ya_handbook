9e178b39f5d5efdb954e357a59fef08bb72d7af9
0xMihalich <bayan-mobile@list.ru>
2022-11-29 22:10:32 +1000

    Update code.py

--- основы_python/3.5/t/code.py
+++ основы_python/3.5/t/code.py
@@ -1,14 +1,7 @@
-# снова получаю ошибку при прохождении тестов, есть предположение на ошибку со стороны яндекс, отправлен запрос в ТП
-
-# решение 1
-with open('numbers.num', 'rb') as f:
-    nums = f.read()
-print(sum([int.from_bytes(nums[i: i + 2], 'big') for i in range(0, len(nums), 2)]))
-
-# решение 2
-from struct import unpack
+from ctypes import c_ushort
 
 
 with open('numbers.num', 'rb') as f:
     nums = f.read()
-print(sum([unpack('>h', nums[i: i + 2])[0] for i in range(0, len(nums), 2)]))
+result = sum([int.from_bytes(nums[i: i + 2], 'big') for i in range(0, len(nums), 2)])
+print(c_ushort(result).value)
