862bfce31bb2964ddba10b41b4fd48e5f4b83ef1
0xMihalich <bayan-mobile@list.ru>
2022-11-29 22:40:08 +1000

    Update code.py

--- основы_python/4.1/j/code.py
+++ основы_python/4.1/j/code.py
@@ -1,6 +1,9 @@
-# Снова ошибка при выполнении функции, хотя функция работает. Так же задан вопрос поддержке
 def merge(tuple_1, tuple_2):
-    return tuple(sorted(list(set(tuple_1) | set(tuple_2))))
-
-assert merge((1, 2), (3, 4, 5)) == (1, 2, 3, 4, 5)
-assert merge((7, 12), (1, 9, 50)) == (1, 7, 9, 12, 50)
+    result = list(tuple_1) + list(tuple_2)
+    n = 1
+    while n < len(result):
+        for i in range(len(result) - n):
+            if result[i] > result[i + 1]:
+                result[i], result[i + 1] = result[i + 1], result[i]
+        n += 1
+    return tuple(result)
