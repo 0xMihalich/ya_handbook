ac39091f720d67e54ea2675f37211c3b69c9b489
0xMihalich <bayan-mobile@list.ru>
2022-11-29 22:18:16 +1000

    Update code.py

--- основы_python/3.5/t/code.py
+++ основы_python/3.5/t/code.py
@@ -3,5 +3,10 @@ from ctypes import c_ushort
 
 with open('numbers.num', 'rb') as f:
     nums = f.read()
-result = sum([int.from_bytes(nums[i: i + 2], 'big') for i in range(0, len(nums), 2)])
-print(c_ushort(result).value)
+
+result = c_ushort()
+
+for i in range(0, len(nums), 2):
+    result.value += c_ushort(int.from_bytes(nums[i: i + 2], 'big')).value
+
+print(result.value)
